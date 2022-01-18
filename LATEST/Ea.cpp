a5e0036708aa6bd113bbba34e9625d2096b0468d
root <nagaraja.hm.0001@gmail.com>
2022-01-18 10:58:48 +0100

    Update: submodule Ea

--- LATEST/Ea.cpp
+++ LATEST/Ea.cpp
@@ -5,3 +5,39 @@ class_Ea Ea;
 FUNC(void, EA_CODE) class_Ea::InitFunction(void){
 }
 
+FUNC(void, EA_CODE) class_Ea::SetMode(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::Read(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::Write(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::Cancel(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::GetStatus(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::GetJobResult(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::InvalidateBlock(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::GetVersionInfo(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::EraseImmediateBlock(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::CbJobEndNotification(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::CbJobErrorNotification(void){
+}
+
+FUNC(void, EA_CODE) class_Ea::MainFunction(void){
+}
+
--- LATEST/Ea.h
+++ LATEST/Ea.h
@@ -5,7 +5,19 @@
 
 class class_Ea{
    public:
-      FUNC(void, EA_CODE) InitFunction(void);
+      FUNC(void, EA_CODE) InitFunction           (void);
+      FUNC(void, EA_CODE) SetMode                (void);
+      FUNC(void, EA_CODE) Read                   (void);
+      FUNC(void, EA_CODE) Write                  (void);
+      FUNC(void, EA_CODE) Cancel                 (void);
+      FUNC(void, EA_CODE) GetStatus              (void);
+      FUNC(void, EA_CODE) GetJobResult           (void);
+      FUNC(void, EA_CODE) InvalidateBlock        (void);
+      FUNC(void, EA_CODE) GetVersionInfo         (void);
+      FUNC(void, EA_CODE) EraseImmediateBlock    (void);
+      FUNC(void, EA_CODE) CbJobEndNotification   (void);
+      FUNC(void, EA_CODE) CbJobErrorNotification (void);
+      FUNC(void, EA_CODE) MainFunction           (void);
 };
 
 extern class_Ea Ea;
