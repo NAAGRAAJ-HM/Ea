884d5374bf29b72d9c1f9c2c8d2ae3d5331a9e08
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:00 +0100

    Update: AUTOSAR basic software ECU abstraction module Ea

--- LATEST/Ea.cpp
+++ LATEST/Ea.cpp
@@ -40,6 +40,16 @@ class module_Ea:
       FUNC(void, EA_CODE) DeInitFunction (void);
       FUNC(void, EA_CODE) GetVersionInfo (void);
       FUNC(void, EA_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, EA_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
