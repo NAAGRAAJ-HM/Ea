e21b2e0aae35f581512b84577fb1a58a379684fd
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:02:59 +0200

    Update: AUTOSAR basic software ECU abstraction module Ea

--- LATEST/Ea.cpp
+++ LATEST/Ea.cpp
@@ -37,10 +37,9 @@ class module_Ea:
    public:
       module_Ea(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, EA_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, EA_CONFIG_DATA, EA_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, EA_CODE) InitFunction   (void);
       FUNC(void, EA_CODE) DeInitFunction (void);
       FUNC(void, EA_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_Ea, EA_VAR) Ea(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, EA_CODE) module_Ea::InitFunction(
-   CONSTP2CONST(CfgEa_Type, CFGEA_CONFIG_DATA, CFGEA_APPL_CONST) lptrCfgEa
+   CONSTP2CONST(CfgModule_TypeAbstract, EA_CONFIG_DATA, EA_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgEa){
+   if(E_OK == IsInitDone){
 #if(STD_ON == Ea_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgEa for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == Ea_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_Ea as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   Ea.IsInitDone = E_OK;
 }
 
 FUNC(void, EA_CODE) module_Ea::DeInitFunction(void){
-   Ea.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == Ea_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, EA_CODE) module_Ea::MainFunction(void){
