8d92ec912fa29d76fbe6ff2406f3230797d71e88
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:01 +0000

    Update: AUTOSAR basic software ECU abstraction module Ea

--- LATEST/Ea.cpp
+++ LATEST/Ea.cpp
@@ -113,7 +113,7 @@ FUNC(void, EA_CODE) module_Ea::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  EA_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, EA_CODE) module_Ea::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  EA_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, EA_CODE) module_Ea::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  EA_E_UNINIT
       );
 #endif
    }
