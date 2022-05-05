400e5cdc2be523b0dcc28a6cef372d535c91e892
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:07:18 +0000

    Update: AUTOSAR basic software ECU abstraction module Ea

--- LATEST/Ea.cpp
+++ LATEST/Ea.cpp
@@ -85,12 +85,10 @@ FUNC(void, EA_CODE) module_Ea::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfg_CanIf as back-up configuration
             lptrCfg = &PBcfgEa;
          }
       }
