dc65abaee68840a088c27c1b3965c879c4ee1aa4
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-08 21:18:34 +0000

    Update: AUTOSAR basic software ECU abstraction module Ea

--- LATEST/Ea.cpp
+++ LATEST/Ea.cpp
@@ -74,7 +74,7 @@ CONSTP2VAR(infSchMClient, EA_VAR, EA_CONST) gptrinfSchMClient_Ea = &Ea;
 VAR(module_Ea, EA_VAR) Ea(
    {
 #if(STD_ON == _ReSIM)
-// char strModuleName[6];
+   char strModuleName[6] = "EA";
 #else
 #endif
          EA_AR_RELEASE_VERSION_MAJOR
