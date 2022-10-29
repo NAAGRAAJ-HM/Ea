668fd361985e32f90173b14a5d125890c70e7cda
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-29 15:52:41 +0200

    Update: AUTOSAR basic software ECU abstraction module for EEPROM abstraction

---
+++ InfineonTLE9879EvalKit_R2022/EcuabEa_core.hpp
@@ -0,0 +1,70 @@
+#pragma once
+/******************************************************************************/
+/* File   : EcuabEa_core.hpp                                                       */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_EcuabEa.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define ECUABEA_CORE_FUNCTIONALITIES                                                \
+              FUNC(void, ECUABEA_CODE) SetMode                (void);               \
+              FUNC(void, ECUABEA_CODE) Read                   (void);               \
+              FUNC(void, ECUABEA_CODE) Write                  (void);               \
+              FUNC(void, ECUABEA_CODE) McalCancel                 (void);               \
+              FUNC(void, ECUABEA_CODE) GetStatus              (void);               \
+              FUNC(void, ECUABEA_CODE) GetJobResult           (void);               \
+              FUNC(void, ECUABEA_CODE) InvalidateBlock        (void);               \
+              FUNC(void, ECUABEA_CODE) EraseImmediateBlock    (void);               \
+              FUNC(void, ECUABEA_CODE) CbJobEndNotification   (void);               \
+              FUNC(void, ECUABEA_CODE) CbJobErrorNotification (void);               \
+
+#define ECUABEA_CORE_FUNCTIONALITIES_VIRTUAL                                        \
+      virtual FUNC(void, ECUABEA_CODE) SetMode                (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) Read                   (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) Write                  (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) McalCancel                 (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) GetStatus              (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) GetJobResult           (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) InvalidateBlock        (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) EraseImmediateBlock    (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) CbJobEndNotification   (void) = 0;           \
+      virtual FUNC(void, ECUABEA_CODE) CbJobErrorNotification (void) = 0;           \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_EcuabEa_Functionality{
+   public:
+      ECUABEA_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
